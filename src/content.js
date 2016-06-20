b295b316dd1af84e9d671f64c6077c0bddde8744
Gustav Bylund <maistho@gmail.com>
2016-06-20 18:46:21 +0200

    Removed unused messages

--- src/content.js
+++ src/content.js
@@ -1,15 +1,3 @@
-chrome.extension.sendMessage({}, function(response) {
-	var readyStateCheckInterval = setInterval(function() {
-		if (document.readyState === "complete") {
-			clearInterval(readyStateCheckInterval);
-
-			// ----------------------------------------------------------
-			// This part of the script triggers when page is done loading
-			console.log("Hello. This message was sent from scripts/content.js");
-			// ----------------------------------------------------------
-		}
-	}, 10);
-});
 var s = document.createElement('script');
 s.src = chrome.extension.getURL('src/inject.js');
 s.onload = function() {
